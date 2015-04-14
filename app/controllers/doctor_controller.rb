3bfc1245abe1205b0cf8141aaa9bacf66c240bae
Leoncio <leoncioperez512@gmail.com>
2015-04-13 22:58:35 -0400

    front end work

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,12 +1,10 @@
 class DoctorController < ApplicationController
   def index
-  	specialty_search = gets.chomp
-  	location = gets.chomp
-  	distance = gets.chomp.to_i
+  	
   end
   def search
-  	parameters = { term: params[:term], limit: 16 }
-    render json: Yelp.client.search('#{user_choice}', parameters)
+  	#parameters = { term: 'doctor', limit: 16, sort: 2,  }
+    #render json: Yelp.client.search(parameters)
   end
   def show
   end
