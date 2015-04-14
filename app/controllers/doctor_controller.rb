7a74beaf8584893b0e085b1b5bfa7ec9f860e25d
Leoncio <leoncioperez512@gmail.com>
2015-04-13 20:32:41 -0400

    ok

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,5 +1,8 @@
 class DoctorController < ApplicationController
   def index
+  	specialty_search = gets.chomp
+  	location = gets.chomp
+  	distance = gets.chomp.to_i
   end
   def search
   	parameters = { term: params[:term], limit: 16 }
