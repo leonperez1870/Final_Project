316eacb52942b5480d69e1b22a74711ade25ea41
Leoncio <leoncioperez512@gmail.com>
2015-04-22 21:21:29 -0400

    search function working

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -6,8 +6,8 @@ class DoctorController < ApplicationController
   	#parameters = { term: 'doctor', limit: 16, sort: 2, category_filter: 'cardiologist', radius_filter: 30000 }
     #render json: Yelp.client.search('New York', parameters)
     parameters = { term: params[:term], limit: 16 }
-    render json: Yelp.client.search('New York', parameters)
+    @results = Yelp.client.search('New York', parameters)
     
   end
-  
+
 end
